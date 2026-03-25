39d724f8f13536c3baa8f5c52b587594974cfe7d
Leejeonghak <qhfxm704@gmail.com>
2026-03-25 10:24:36 +0900

    나만의 프로필 api(json 반환)

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return "Hello, World"
+def my_profile():
+    return {
+        "name": "홍길동",
+        "role": "초보 서버 개발자",
+        "status": "로컬 환경 통제 완료!",
+        "skills": ["Ubuntu", "VS Code", "Python", "Flask"]
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
