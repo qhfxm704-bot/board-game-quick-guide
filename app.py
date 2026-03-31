767d96b9dafc2d78629b0737271bbb243c9019dc
Leejeonghak <qhfxm704@gmail.com>
2026-04-01 08:48:20 +0900

    HTML 파일 보내기

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def my_profile():
-    return {
-        "name": "홍길동",
-        "role": "초보 서버 개발자",
-        "status": "로컬 환경 통제 완료!",
-        "skills": ["Ubuntu", "VS Code", "Python", "Flask"]
-    }
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
