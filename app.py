831e0b8be0c9697d1cb1cf6b3a23d29137a17b94
Leejeonghak <qhfxm704@gmail.com>
2026-04-01 09:24:53 +0900

    데이터를 HTML 화면에 표시

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    my_profile = {
+        "name": "이정학",
+        "age": 18,
+        "school": "종로산업정보학교",
+        "hobby": "게임",
+        "email": "qhfxm704@gmail.com",
+        "phone": "010-8687-3510",
+        "dream": "개발자",
+    }
+    return render_template('index.html', data=my_profile)
 
 if __name__ == '__main__':
     app.run(debug=True)
