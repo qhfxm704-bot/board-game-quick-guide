30edecdccd4e140a0f6b528e50da3e194b29ed2a
Leejeonghak <qhfxm704@gmail.com>
2026-04-15 09:13:05 +0900

    260415 저장 및 목록 출력

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+messages = []
 
 @app.route('/')
 def home():
     skills = [
-        "C", "C++", "C#", "Java", "Kotlin",
-        "JavaScript", "TypeScript",
-        "Python", "Rust", "Go",
-        "Ruby", "PHP",
-        "HTML", "CSS", "Markdown",
-        "SQL", "R",
-        "Bash", "Shell",
-        "Dart", "Swift"
+        "Python", "HTML", "SQL", "Bash"
     ]
-    return render_template('index.html', data=skills)
+    return render_template('index.html', data=skills, messages=messages)
 
 @app.route('/send', methods=['POST'])
 def send():
     skill = request.form.get('skill')
     level = request.form.get('level')
     status = request.form.get('status')
-    return skill + " / " + level + " / " + status
+
+    messages.append(skill + " / " + level + " / " + status)
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
